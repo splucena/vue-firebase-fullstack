2cd98e93899e5da93165a61c6ef2d309a64c5647
splucena <sumilangplucena@yahoo.com.ph>
2020-12-28 14:19:43 +0800

    added navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<nav class="site-nav navbar navbar-expand bg-primary navbar-dark">
+			<div class="container-fluid">
+				<router-link class="navbar-brand" to="/"
+					>Meeting Log</router-link
+				>
+				<div class="navbar-nav ml-auto">
+					<router-link class="nav-item nav-link" to="/login"
+						>Login</router-link
+					>
+					<router-link class="nav-item nav-link" to="/meetings"
+						>Meetings</router-link
+					>
+					<router-link class="nav-item nav-link" to="/register"
+						>Register</router-link
+					>
+				</div>
+			</div>
+		</nav>
+	</div>
+</template>
+<script>
+export default {};
+</script>
